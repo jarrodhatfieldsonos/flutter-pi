6eba85c5d07edfc0bf8796f92c4c9c2a4169d81a
jarrodhatfieldsonos <jarrod.hatfield@sonos.com>
2020-06-17 13:48:20 -0700

    added engine binaries for the various runtime modes

--- scripts/make_flutter.sh
+++ scripts/make_flutter.sh
@@ -1,23 +1,24 @@
 #! /bin/bash
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -eq 2 ]; then
     ROOT_DIR=$(realpath $1)
+    RUNTIME_MODE=${2,,}
 else
-    echo "usage: make_flutter <root_dir>"
+    echo "usage: make_flutter <root_dir> [release|profile|debug]"
     exit 1
 fi
-
-sudo cp $ROOT_DIR/binaries/libflutter_engine.so $ROOT_DIR/binaries/icudtl.dat /usr/lib
-sudo cp $ROOT_DIR/binaries/flutter_embedder.h /usr/include
 
 cd $ROOT_DIR
 
 cmake . \
--DCMAKE_BUILD_TYPE=Debug \
--DFLUTTER_ENGINE_LIBRARY=$ROOT_DIR/binaries/libflutter_engine.so \
+-DCMAKE_BUILD_TYPE=Release \
+-DFLUTTER_ENGINE_LIBRARY=$ROOT_DIR/binaries/$RUNTIME_MODE/libflutter_engine.so \
 -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=out \
 -DCMAKE_INSTALL_PREFIX=/usr/local
 
 if [ $? -eq 0 ] ; then
+    sudo cp $ROOT_DIR/binaries/$RUNTIME_MODE/libflutter_engine.so $ROOT_DIR/binaries/$RUNTIME_MODE/icudtl.dat /usr/lib
+    sudo cp $ROOT_DIR/binaries/$RUNTIME_MODE/flutter_embedder.h /usr/include
+
     sudo make install
 fi
